897e0a8a41e659a66736372c7300b02ca3a8360a
Deepak Chauhan <am.deepakkumarchauhan@gmail.com>
2024-07-04 09:00:05 +0530

    Added error files

---
+++ src/utils/errors/service-error.js
@@ -0,0 +1,16 @@
+const { StatusCodes } = require("http-status-code");
+
+class ServiceError extends Error {
+  constructor(
+    message = "Something went wrong",
+    explanation = "Service layer error occurred",
+    statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+  ) {
+    this.message = message;
+    this.name = "ServiceError";
+    this.explanation = explanation;
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = ServiceError;
