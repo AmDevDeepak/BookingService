e8407926ba36a3cbb3e74f41107e0ab4bc05f55e
Deepak Chauhan <am.deepakkumarchauhan@gmail.com>
2024-07-06 16:01:05 +0530

    Added rabbit MQ config

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,27 +1,41 @@
 const { BookingService } = require("../services/index");
-const bookingService = new BookingService();
 const { StatusCodes } = require("http-status-codes");
-const create = async (req, res) => {
-  try {
-    const response = await bookingService.createBooking(req.body);
-    console.log("From booking controller: " + response)
+const bookingService = new BookingService();
+
+const { REMINDER_BINDING_KEY } = require("../config/serverConfig");
+const { createChannel, publishMessage } = require("../utils/messageQueue");
+
+class BookingController {
+  constructor() {}
+  async sendMessageToQueue(req, res) {
+    const channel = await createChannel();
+    const data = { message: "Success" };
+    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
     return res.status(StatusCodes.OK).json({
-      message: "Booking created successfully",
-      data: response,
+      message: "Successfully published the event",
       success: true,
       err: {},
     });
-  } catch (error) {
-    console.log("From booking controller: " + error)
-    return res.status(error.statusCode).json({
-      data: {},
-      success: false,
-      message: error.message,
-      err: error.explanation,
-    });
   }
-};
+  async create(req, res) {
+    try {
+      const response = await bookingService.createBooking(req.body);
+      console.log("From booking controller: " + response);
+      return res.status(StatusCodes.OK).json({
+        message: "Booking created successfully",
+        data: response,
+        success: true,
+        err: {},
+      });
+    } catch (error) {
+      return res.status(error.statusCode).json({
+        data: {},
+        success: false,
+        message: error.message,
+        err: error.explanation,
+      });
+    }
+  }
+}
 
-module.exports = {
-  create,
-};
+module.exports = BookingController;
