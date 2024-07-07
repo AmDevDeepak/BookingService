583243af93226c2fe9bd2c3058a6ddbe510eaca6
Deepak Chauhan <am.deepakkumarchauhan@gmail.com>
2024-07-07 12:47:09 +0530

    API gateway

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
 const apiRoutes = require("./routes/index");
-const db = require("./models/index")
+const db = require("./models/index");
 
 const setupAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-
+  // app.get("/api/v1/home", (req, res) => {
+  //   return res.json({
+  //     message: "Hitting the booking service...",
+  //     success: true,
+  //   });
+  // });
   app.use("/api", apiRoutes);
 
   app.listen(PORT, () => {
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const { BookingController } = require("../../controllers/index");
 
-// const { createChannel } = require("../../utils/messageQueue");
-// const channel = await createChannel();
-
 const bookingController = new BookingController();
 
 const router = express.Router();
 
 router.post("/bookings", bookingController.create);
 router.post("/publish", bookingController.sendMessageToQueue);
+router.get('/info', (req, res) => {
+    return res.json({
+        message: "Response from bookings routes",
+        success: true
+    })
+})
 module.exports = router;
